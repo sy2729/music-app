807271b65f5acdb25561c1da22ef6fd5cf89e152
sy2729 <sy2729@tc.columbia.edu>
2018-07-16 23:13:25 -0400

    add MVC for list recommendation

---
+++ src/js/app/list-recommendation.js
@@ -0,0 +1,92 @@
+{
+    let view = {
+        el: '#list-recommen',
+        template: `
+         <ul>
+            <li>
+                <img src="http://via.placeholder.com/246" alt="">
+                <p class="list-recommend-descrip">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                <div class="list-recommend-info-wrap">
+                    <i class="list-recommend-info-wrap-icon iconfont">&#xe645;</i>
+                    <span class="list-recommend-info-wrap-text">1213M</span>
+                </div>
+            </li>
+        </ul>
+        `,
+        render(data) {
+            $(this.el).html(this.template);
+            let lis = [];
+            data.forEach((i)=>{
+                let img = $(`<img>`).attr('src', i.imageUrl);
+                let p = $(`<p></p>`).addClass('list-recommend-descrip').text(i.title);
+                let iEl = $(`<i></i>`).addClass('list-recommend-info-wrap-icon iconfont').html('&#xe645;');
+                let span = $(`<span></span>`).addClass('list-recommend-info-wrap-text').text(i.listened);
+                let div = $(`<div></div>`).addClass('list-recommend-info-wrap').append(iEl).append(span);
+                let li = $(`<li></li>`).append(img).append(p).append(div);
+
+                lis.push(li);
+            })
+            let content = this.template;
+            $(this.el).find('ul').empty().html($(content).empty().append(lis))
+        }
+    };
+
+    let model = {
+        data: {
+            listRecom: [
+                {
+                    title: 'Lorem ipsum dolor sit amet',
+                    imageUrl: 'http://via.placeholder.com/246',
+                    listened: 2000
+                },
+                {
+                    title: 'Lorem ipsum dolor sit amet',
+                    imageUrl: 'http://via.placeholder.com/246',
+                    listened: 2000
+                },
+                {
+                    title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+                    image: 'http://via.placeholder.com/246',
+                    listened: 2000
+                },
+                {
+                    title: 'Lorem ipsum dolor sit amet',
+                    image: 'http://via.placeholder.com/246',
+                    listened: 2000
+                },
+                {
+                    title: 'Lorem ipsum dolor sit amet',
+                    image: 'http://via.placeholder.com/246',
+                    listened: 2000
+                },
+                {
+                    title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+                    image: 'http://via.placeholder.com/246',
+                    listened: 2000
+                },
+            ]
+        },
+
+        update() {
+
+        }
+
+    };
+
+    let controller = {
+        init(view, model){
+            this.view = view;
+            this.model = model;
+            this.view.render(this.model.data.listRecom);
+            this.bindEvents();
+        },
+
+        bindEvents(){
+
+        },
+
+    }
+
+    controller.init(view, model);
+
+}
