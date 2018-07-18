d65130af0781cf5c191f090f9b03b60b94584740
sy2729 <sy2729@tc.columbia.edu>
2018-07-18 00:36:07 -0400

    add play page and basic play and pause function

--- src/js/app/play-song.js
+++ src/js/app/play-song.js
@@ -1,15 +1,39 @@
  {
      let view = {
-        el: '',
-        template: '',
-        render() {
+        el: '#song',
+        template: `
+            <audio src="{{songUrl}}"></audio>
+            <button class="play">play</button>
+            <button class="pause">pause</button>
+        `,
+        render(data) {
+            let content = this.template.replace("{{songUrl}}", data.link);
+            $(this.el).html(content);
+        },
 
+        play() {
+            console.log(111)
+            $(this.el).find('audio')[0].play();
+        },
+
+        pause() {
+            $(this.el).find('audio')[0].pause();
         }
      };
 
      let model = {
-        data: {},
-
+        data: {
+            id: '',
+            name: '',
+            singer: '',
+            link: ''
+        },
+        getSongData(id){
+            var query = new AV.Query('Song');
+            return query.get(id).then((data)=>{
+                Object.assign(this.data, { id: data.id, ...data.attributes})
+            })
+        }
 
         
      };
@@ -18,18 +42,32 @@
          init(view, model) {
             this.view = view;
             this.model = model;
-            this.view.render();
-            let id = this.getSongData();
-            console.log(id)
+            let id = this.getSongId();
+            this.model.getSongData(id)
+                .then(()=>{
+                    this.view.render(this.model.data);
+                });
             this.bindEvents();
          },
 
          bindEvents(){
-            
+            this.playSong();
+            this.pauseSong();
          },
 
-
-         getSongData() {
+         playSong(){
+            //  console.log($(this.view.el))
+            $(this.view.el).on('click', '.play', ()=>{
+                console.log(222)
+                this.view.play();
+            })
+         },
+         pauseSong(){
+             $(this.view.el).on('click','.pause',() => {
+                 this.view.pause();
+             })
+         },
+         getSongId() {
              let query = window.location.search;
              if (query.indexOf('?') === 0) {
                  query = query.substring(1)
