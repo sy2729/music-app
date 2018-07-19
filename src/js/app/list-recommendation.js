40d76f17aa4c396711e09fb0e871962203c6423e
sy2729 <sy2729@tc.columbia.edu>
2018-07-18 21:31:43 -0400

    instead using png

--- src/js/app/list-recommendation.js
+++ src/js/app/list-recommendation.js
@@ -33,43 +33,40 @@
 
     let model = {
         data: {
-            listRecom: [
-                {
-                    title: 'Lorem ipsum dolor sit amet',
-                    imageUrl: 'http://p1.music.126.net/tk4L-mj-QoIBlj4zYQZJlQ==/109951163410830065.webp?imageView&thumbnail=246x0&quality=75&tostatic=0&type=webp',
-                    listened: 2000
-                },
-                {
-                    title: 'Lorem ipsum dolor sit amet',
-                    imageUrl: 'http://p1.music.126.net/P1IXTC2djtA89w7SDaTMyQ==/109951163391859286.webp?imageView&thumbnail=246x0&quality=75&tostatic=0&type=webp',
-                    listened: 2000
-                },
-                {
-                    title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-                    imageUrl: 'http://p1.music.126.net/YVgW0O5SijOq0m8C90PavQ==/18894007812317570.webp?imageView&thumbnail=246x0&quality=75&tostatic=0&type=webp',
-                    listened: 2000
-                },
-                {
-                    title: 'Lorem ipsum dolor sit amet',
-                    imageUrl: 'http://p1.music.126.net/2fypBTo8GNQ7QiEs41INDw==/19182079858548695.webp?imageView&thumbnail=246x0&quality=75&tostatic=0&type=webp',
-                    listened: 2000
-                },
-                {
-                    title: 'Lorem ipsum dolor sit amet',
-                    imageUrl: 'http://p1.music.126.net/JjPFgHX0Ptzj79kfZJ6Uag==/18631224534867980.webp?imageView&thumbnail=246x0&quality=75&tostatic=0&type=webp',
-                    listened: 2000
-                },
-                {
-                    title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-                    imageUrl: 'http://p1.music.126.net/VLHkaCwI_V8yOKWt0J9sFA==/18554258720544063.webp?imageView&thumbnail=246x0&quality=75&tostatic=0&type=webp',
-                    listened: 2000
-                },
-            ]
+            listRecom: [{
+                title: 'Lorem ipsum dolor sit amet',
+                imageUrl: '1',
+                listened: 2000
+            }, {
+                title: 'Lorem ipsum dolor sit amet',
+                imageUrl: '2',
+                listened: 2000
+            }, {
+                title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+                imageUrl: '3',
+                listened: 2000
+            }, {
+                title: 'Lorem ipsum dolor sit amet',
+                imageUrl: '4',
+                listened: 2000
+            }, {
+                title: 'Lorem ipsum dolor sit amet',
+                imageUrl: '5',
+                listened: 2000
+            }, {
+                title: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+                imageUrl: '6',
+                listened: 2000
+            }]
         },
 
-        update() {
+        init() {
+            this.data.listRecom.map((i) => {
+                i.imageUrl = `./dist/img/temp_${i.imageUrl}.png`
+            })
+        },
 
-        }
+        update() { }
 
     };
 
@@ -77,6 +74,7 @@
         init(view, model){
             this.view = view;
             this.model = model;
+            this.model.init();
             this.view.render(this.model.data.listRecom);
             this.bindEvents();
         },
